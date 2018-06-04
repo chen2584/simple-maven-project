8ed2ec6d9fac1c198d125c2467067245826321de
Worameth Semapat <worameth.semapat@gmail.com>
2018-06-04 17:12:14 +0700

    Add simple restcontroller

---
+++ src/main/java/io/javabrains/springbootstarter/hello/HelloController.java
@@ -0,0 +1,15 @@
+package io.javabrains.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	//@RequestMapping(value = "/hello", method = RequestMethod.POST)
+	@RequestMapping("/hello") //GET
+	public String sayHi() {
+		return "Hi";
+	}
+}
