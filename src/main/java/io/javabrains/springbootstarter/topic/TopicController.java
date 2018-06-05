644518bd586e3e648ce533fad66a495b56763659
Worameth Semapat <worameth.semapat@gmail.com>
2018-06-05 19:51:21 +0700

    Add Services

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -3,20 +3,20 @@ package io.javabrains.springbootstarter.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		
-		return Arrays.asList(
-				new Topic("spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("java", "Core Java", "Java Description"),
-				new Topic("javascript", "JavaScript", "JavaScript Description")
-				);
+		return topicService.getAllTopics();
 	}
 }
  
